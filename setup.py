b9843d29da334d957fe370a6333a89d18f71d53c
Andrew Langemann <andrew.langemann@arcurve.com>
2022-03-01 23:04:23 +0000

    Add more Python versions to setup.py

--- setup.py
+++ setup.py
@@ -17,6 +17,9 @@ CLASSIFIERS = [
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
 ]
 INSTALL_REQUIRES = ['requests>=2.7.0']
 
